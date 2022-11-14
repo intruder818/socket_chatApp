c98af155dd73332fce491c9565a1f32b5bbd3eec
intruder818 <omdatanikhil@gmail.com>
2022-11-14 11:38:49 +0530

    socket working

--- Server/index.js
+++ Server/index.js
@@ -1,31 +1,60 @@
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const cors = require('cors');
+// const http = require('http');
+// const express = require('express');
+// const socketio = require('socket.io');
+// var cors = require('cors')
 
-const router=require("./router")
 
-const app = express();
+
+// const app = express();
+// app.use(cors())
+// const server = http.createServer(app);
+// const io = socketio(server);
+// const PORT=5000
+
+
+// const router=require("./router")
+// app.use(router)
+const express=require("express")
+const app=express()
+const PORT=8000
+// var server  = app.listen(PORT);
+const http = require("http").Server(app)
+var io = require("socket.io" )(http, {
+  cors: {
+      origin: "http://localhost:3000"
+  }
+});
+var cors = require('cors')
 app.use(cors())
-const server = http.createServer(app);
-const io = socketio(server);
-const PORT=5000
-
-app.use(router)
 
 
 
 
 
 
+app.get("/",(req,res)=>{
+    res.sendFile( __dirname + "/index.html");
+})
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
 
 
 
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+
+
+
+
+io.on( "connection", function( socket ) {
+  console.log("a user has connected!" );
+  socket.on( "disconnect", function() {
+  console.log( "user disconnected" );
   });
 
+  socket.on( "upvote-event", function( upvote_flag ) {
+      console.log( "upvote:" + upvote_flag );
+      });
 
-
-server.listen(process.env.PORT || 5000, () => console.log(`Server has started on ${PORT}`));
+  });
